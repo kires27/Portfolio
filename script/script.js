a09fef95310f84ad71a5035a8bd141c8716d1ae2
kire27 <kires27@gmail.com>
2022-12-06 15:36:36 +0100

    6 dec

---
+++ script/script.js
@@ -0,0 +1,5 @@
+const container = document.getElementById('containerP');
+const chocos = document.getElementById('chocos');
+
+const count = container.getElementsByTagName('div').length;
+chocos.innerText = count;
