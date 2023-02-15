9029abe54e99a2ebab989430b674480a9b147503
kire27 <kires27@gmail.com>
2023-02-15 22:45:06 +0100

    automation

--- script/script.js
+++ script/script.js
@@ -1,5 +1,59 @@
-const container = document.getElementById('containerP');
-const chocos = document.getElementById('chocos');
+const chocos = document.getElementById("chocos");
+const project_list = document.getElementById("project-list");
+const project_cards = document.getElementById("project-cards");
 
-const count = container.getElementsByTagName('div').length;
-chocos.innerText = count;
+const url = "https://api.github.com/users/kire27/repos";
+
+fetchJsonData(url, project_list, project_cards);
+
+async function fetchJsonData(jsonUrl, listParent, cardParent) {
+    const response = await fetch(jsonUrl);
+    const data = await response.json();
+
+    const fileResponse = await fetch("../res/addInfo.json");
+    const fileData = await fileResponse.json();
+
+    buildList(data, listParent);
+    BuildCards(data, fileData, cardParent);
+}
+
+function buildList(data, listParent) {
+    if (!data) parentEl.style.display = "none";
+    console.log(data);
+
+    chocos.innerText = data.length;
+
+    for (let key in data) {
+        let anchor = document.createElement("a");
+        Object.assign(anchor, {
+            text: data[key].name,
+            href: "#" + data[key].name,
+        });
+        listParent.appendChild(anchor);
+    }
+}
+
+function BuildCards(data, fileData, cardParent) {
+    if (!data) parentEl.style.display = "none";
+    // console.log(fileData["Alimeli"]["webpage"]);
+
+    for (let key in data) {
+        for (let kez in fileData) {
+            if ((data[key].name = fileData[data[key].name])) {
+                let name = data[key];
+                let webpage = fileData[data[key].name];
+                console.log(name);
+                let card = `
+                <div>
+                    <a class="wlink" href="https://www.domath.haam.space">DoMath</a>
+                    <a class="githl" href="https://github.com/kire27/basic-math-problem-generator">
+                        <img class="githt cat5" src="./res/gh.png" alt="haam">
+                    </a>
+                </div>
+                `;
+
+                cardParent.innerHTML += card;
+            }
+        }
+    }
+}
