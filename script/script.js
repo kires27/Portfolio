5697120ac6387c915dab037c03024c4cfd5e67c5
kire27 <kires27@gmail.com>
2023-02-20 17:12:05 +0100

    remake

--- script/script.js
+++ script/script.js
@@ -1,30 +1,116 @@
-const chocos = document.getElementById("chocos");
+const amount = document.getElementById("amount");
 const project_list = document.getElementById("project-list");
 const project_cards = document.getElementById("project-cards");
+const footer = document.getElementById("footerC");
+
+const arr_left = document.getElementById("arr-left");
+const arr_right = document.getElementById("arr-right");
+const arr_top = document.getElementById("arr-top");
+
+const filter = document.getElementById("list-filter")
 
 const url = "https://api.github.com/users/kire27/repos";
 
+// TODO scroll project_cards when side buttons are held
+
+project_list.onclick = (e) => {
+    const name = e.target.innerHTML;
+    const object = document.getElementById(name);
+    const orgProp = 0.8;
+
+    object.style.opacity = 0.96;
+    object.style.transitionDuration = "1s"
+    
+    setTimeout(() => {
+        object.style.opacity = orgProp;
+    }, 1500);
+}
+
+arr_top.onclick = () => {
+    window.scroll(0, -window.scrollY);
+}
+
+arr_left.onclick = () => {
+    project_cards.scrollBy(-285, 0);
+}
+
+arr_right.onclick = () => {
+    project_cards.scrollBy(285, 0);
+}
+
+window.onresize = () => {
+    if(window.innerWidth < 700) project_cards.scroll(0, 0);
+}  
+
+footer.innerText = `© ${new Date().getFullYear()} Haam apps`;
+
 fetchJsonData(url, project_list, project_cards);
+
+filter.oninput = () => {
+    filterCards(filter.value, project_cards);
+}
+
+function filterCards(filter, project_cards) {
+    const tags = project_cards.querySelectorAll('[class=card-languages]')
+    const allTags = project_cards.querySelectorAll('[id=clg]')
+    const filterLow = filter.toLowerCase();
+
+    let allLanguages = [];
+
+    // get all languages into allLanguages array
+    for (let lg of allTags) {
+        if (!allLanguages.includes(lg.innerText.toLowerCase())) 
+            allLanguages.push(lg.innerText.toLowerCase());
+    }
+
+    for (let tag=0; tag<tags.length; tag++) {
+        
+        // check if the allLanguages contains word from input
+        if (!allLanguages.includes(filterLow)) {
+            (project_cards.children[tag]).classList.remove("filtered")
+            continue
+        };
+        
+        let languages = [];
+
+        // get card languages into array
+        for (let lg=0; lg<tags[tag].children.length; lg++) {
+            languages.push(
+                tags[tag].children[lg].innerText.toLowerCase()
+            )
+        }
+
+        if(!(languages.includes(filterLow))) {
+            (project_cards.children[tag]).classList.add("filtered")
+        } else {
+            (project_cards.children[tag]).classList.remove("filtered")
+        }
+    }
+
+}
 
 async function fetchJsonData(jsonUrl, listParent, cardParent) {
     const response = await fetch(jsonUrl);
     const data = await response.json();
 
-    const fileResponse = await fetch("../res/addInfo.json");
-    const fileData = await fileResponse.json();
-
-    buildList(data, listParent);
-    BuildCards(data, fileData, cardParent);
+    BuildSkeletonCard(data.length, cardParent);
+    BuildList(data, listParent);
+    BuildCards(data);
 }
 
-function buildList(data, listParent) {
-    if (!data) parentEl.style.display = "none";
+function BuildList(data, listParent) {
+    if (!data) {
+        listParent.innerText = "Data not found!";
+        return;
+    }
     console.log(data);
 
-    chocos.innerText = data.length;
+    amount.innerText = data.length;
 
     for (let key in data) {
         let anchor = document.createElement("a");
+        let hr = document.createElement("hr");
+
         Object.assign(anchor, {
             text: data[key].name,
             href: "#" + data[key].name,
@@ -33,27 +119,83 @@ function buildList(data, listParent) {
     }
 }
 
-function BuildCards(data, fileData, cardParent) {
-    if (!data) parentEl.style.display = "none";
-    // console.log(fileData["Alimeli"]["webpage"]);
+async function BuildCards(data, filter) {
+    if (!data) return;
 
     for (let key in data) {
-        for (let kez in fileData) {
-            if ((data[key].name = fileData[data[key].name])) {
-                let name = data[key];
-                let webpage = fileData[data[key].name];
-                console.log(name);
-                let card = `
-                <div>
-                    <a class="wlink" href="https://www.domath.haam.space">DoMath</a>
-                    <a class="githl" href="https://github.com/kire27/basic-math-problem-generator">
-                        <img class="githt cat5" src="./res/gh.png" alt="haam">
-                    </a>
-                </div>
-                `;
+        const rel_res = await fetch(data[key]["releases_url"].slice(0, -5));
+        const rel_data = await rel_res.json();
 
-                cardParent.innerHTML += card;
-            }
+        const lan_res = await fetch(data[key]["languages_url"]);
+        const lan_data = await lan_res.json();
+
+        let xData = {
+            name: data[key]["name"],
+            size: Math.round((data[key]["size"]/1024)*10)/10,
+            homepage: data[key]["homepage"],
+            created: String(new Date(data[key]["created_at"])).split(" "),
+            html: data[key]["html_url"],
+            description: data[key]["description"],
+            topics: "",
+            releases: rel_data[0]?.name == undefined ? "none" : rel_data[0]?.name,
+            languages: "",
+        };
+
+        for (let topic in data[key]["topics"]) {
+            xData.topics += `<span>${data[key]["topics"][topic]}</span>`;
         }
+
+        for (let [key, value] of Object.entries(lan_data)) {
+            xData.languages += `<span id="clg">${key}</span>`;
+        }
+
+        let card = `
+        <div id="${xData.name}" class="card">
+            <div class="card-source">
+                <a ${xData.homepage ? `href="${xData.homepage}"` : ""} 
+                    class="card-name">${xData.name}</a>
+                
+                    <a href="${xData.html}">
+                    <img src="./res/icons/github.svg" alt="haam">
+                </a>   
+            </div>
+            
+            <div class="card-created">
+                <div>
+                    ${xData.created[2]}. ${xData.created[1]} ${xData.created[3]}
+                </div>
+                
+                <div class="card-releases">
+                    <div>release: ${xData.releases}</div>
+                    <div class="card-size">${xData.size}MB</div>
+                </div>
+            </div>
+
+            <div class="card-description">${xData.description}</div>
+            <div class="card-languages">${xData.languages}</div>
+            <div class="card-topics">${xData.topics}</div>
+        </div>
+        `;
+
+        document.getElementById(`skeleton${Number(key)}`)
+        .replaceWith(stringToHTML(card));
     }
 }
+
+function BuildSkeletonCard(amount, cardParent) {
+    for(let i=0; i<amount; i++) {
+        cardParent.innerHTML += `
+            <div id="skeleton${i}" class="skeleton-card">
+                <div class="skelet1"></div>
+                <div class="skelet2"></div>
+                <div class="skelet3"></div>
+            </div>
+        `
+    }
+}
+
+const stringToHTML = (str) => {
+	var parser = new DOMParser();
+    var doc = parser.parseFromString(str, 'text/html');
+    return doc.body.firstChild;
+};
